3b0e1e6acebb1473b1afdf1a7d8432f551ecea7b
De Wet Blomerus <dewet@blomerus.org>
2021-07-03 09:02:07 -0500

    start walking to grandmother

--- lib/madlib_web/controllers/page_controller.ex
+++ lib/madlib_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule MadlibWeb.PageController do
   use MadlibWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", name_color: Faker.Color.name())
   end
 end
--- lib/madlib_web/views/page_view.ex
+++ lib/madlib_web/views/page_view.ex
@@ -1,3 +1,23 @@
 defmodule MadlibWeb.PageView do
   use MadlibWeb, :view
+
+  defp person_adjective() do
+    ~w(tall short funny generous mean old young plump skinny selfish greedy kind bumpy lumpy)
+    |> Enum.random()
+  end
+
+  defp road_adjective() do
+    ~w(cobblestone dirt yellow-brick straight winding)
+    |> Enum.random()
+  end
+
+  defp geography() do
+    ~w(woods prairy forest city town country farmland beach mountains)
+    |> Enum.random()
+  end
+
+  defp animal() do
+    ~w(wolf beaver platapous duck tiger leopard mouse squirrel cocroach beatle butterfly elephant crocodile ostrich snake chicken hippo pig sheep cow donkey lizzard dinosaur)
+    |> Enum.random()
+  end
 end
